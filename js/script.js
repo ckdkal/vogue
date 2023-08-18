4544bc787d8ca28ec1e58cea14bcd4b0a2bb657f
ckdkal <126321248+ckdkal@users.noreply.github.com>
2023-08-18 12:09:52 +0900

    보그 업로드

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".burger").click(
+    function(){
+        $(".burger").toggleClass("on");
+        $(".v_gnb").toggleClass("on");
+    }
+);
+// $(".burger").click(
+//     function(){
+//         $(".v_gnb").stop().slideDown();
+//     },
+//     function(){
+//         $(".v_gnb").stop().slideUp();
+//     }
+// );
